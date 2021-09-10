693b25d040259e8fb234ba329270fbbed0f22ff9
Joseph Schander <josephSchander@gmail.com>
2021-09-10 11:38:50 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        Scanner uInput = new Scanner(System.in);
+        System.out.println( "What is the first number?" );
+        int Num1 = uInput.nextInt();
+        System.out.println( "What is the second number?" );
+        int Num2 = uInput.nextInt();
+        System.out.println(Num1+"+"+ Num2+"="+(Num1+Num2)+"\n"+Num1+"-"+Num2+"="+(Num1-Num2)+"\n"+Num1+"*"+Num2+"="+(Num1*Num2)+"\n"+Num1+"/"+Num2+"="+(Num1/Num2));
+    }
+}
